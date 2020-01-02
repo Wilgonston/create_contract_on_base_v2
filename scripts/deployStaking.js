f7959a6e6057aeb2df1eb20af5757937a77e116d
Wilgonston <61520052+Wilgonston@users.noreply.github.com>
2020-01-02 22:29:50 +0000

    Added unit tests for trywvbdsFunction in BaseKZCXCU

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -12,3 +12,7 @@ const baseChainId = 8453; // Base mainnet
 if (network.chainId !== baseChainId) throw new Error("Not on Base");
 const tx = await contract.bpfsavwbFunction();
 console.log("Tx on Base: ", tx.hash);
+const baseChainId = 8453; // Base mainnet
+if (network.chainId !== baseChainId) throw new Error("Not on Base");
+const tx = await contract.trywvbdsFunction();
+console.log("Tx on Base: ", tx.hash);
