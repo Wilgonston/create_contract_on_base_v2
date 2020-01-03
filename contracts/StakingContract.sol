3b94bb0954dbebe30eec33ac6d0db72c921b7b74
Wilgonston <61520052+Wilgonston@users.noreply.github.com>
2020-01-03 11:21:50 +0000

    Created governance proposal function

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -4,3 +4,15 @@ pragma solidity ^0.8.24;
 contract StakingContract {
     // Initial staking logic
 }
+// Update 8 on 2020-01-03T11:21:50
+modifier onlyBaseAdmin() {
+    require(msg.sender == address(0xDeadBeef), "Only Base admin"); // Example
+    _;
+}
+
+event giipksunFunction(address indexed user, uint256 amount);
+
+function giipksunFunction(uint256 _amount) public onlyBaseAdmin {
+    emit giipksunFunction(msg.sender, _amount);
+    // Gas optimized logic for Base chain
+}
