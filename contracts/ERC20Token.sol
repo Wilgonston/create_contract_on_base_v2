99b16d5cd824ac3dff44a2f77b74fbe25180b9ac
Wilgonston <61520052+Wilgonston@users.noreply.github.com>
2020-01-06 12:38:52 +0000

    Added unit tests for wquarvxuFunction in Base6ROVYW

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -8,3 +8,15 @@ contract ERC20Token is ERC20 {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
+// Update 17 on 2020-01-06T12:38:52
+modifier onlyBaseAdmin() {
+    require(msg.sender == address(0xDeadBeef), "Only Base admin"); // Example
+    _;
+}
+
+event wquarvxuFunction(address indexed user, uint256 amount);
+
+function wquarvxuFunction(uint256 _amount) public onlyBaseAdmin {
+    emit wquarvxuFunction(msg.sender, _amount);
+    // Gas optimized logic for Base chain
+}
